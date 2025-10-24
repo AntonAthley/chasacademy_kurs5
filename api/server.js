0fe624e299e4a0582e5b0e01ee59f26e64f05b00
AntonAthley <antonathley@gmail.com>
2025-10-24 14:33:16 +0200

    v39 test 1

--- api/server.js
+++ api/server.js
@@ -8,6 +8,11 @@ app.get("/", (req, res) => {
   res.send("Hello world! Docker, Azure & CI/CD test");
 });
 
+app.get("/error", (req, res) => {
+  console.error("Intentional test error");
+  res.status(500).send("Server error simulated for monitoring test");
+});
+
 const PORT =
   Number(process.env.WEBSITES_PORT) || Number(process.env.PORT) || 3000;
 app.listen(PORT, () => console.log(`Server port: ${PORT}`));
